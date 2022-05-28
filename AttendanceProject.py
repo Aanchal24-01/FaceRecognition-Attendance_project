f9b1b89d79c54c0f5d8bbd3a229751bb12f957ba
AANCHAL <85822746+Aanchal24-01@users.noreply.github.com>
2022-05-28 08:25:45 +0530

    Update AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -8,17 +8,26 @@ import face_recognition
 import os
 from datetime import datetime
 
+#creating list for the images so we dont have to write manually each time 
 path='imagesAttendance'
 images=[]
 classNames=[]
 myList=os.listdir(path)
+
+#printing the list
 print(myList)
+
+#importing images one by one using for loop
 for cl in myList:
    curImg=cv2.imread(f'{path}/{cl}')
    images.append(curImg)
+   #we are appending here so that list  will not get printed with extension 
    classNames.append(os.path.splitext(cl)[0])
+
+#printing the list again 
 print(classNames)
 
+# till  now we have found our image  now we have to find the encodings of the image using the findEncodings( ) function
 def findEncodings(images):
     encodeList=[]
     for img in images:
@@ -27,6 +36,7 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
+#for tracking the attendace in excel sheet
 def markAttendence(name):
     with open('Attendance.csv','r+') as f:
         myDataList=f.readlines()
@@ -39,10 +49,13 @@ def markAttendence(name):
             dtString=now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
 
-
+#to run the function findEncodings calling is done here
 encodeListKnown=findEncodings(images)
+
+#to find it works properly print here
 print('Encoding Complete')
 
+#as we dont have second image to have encoding to campre with fisrt one it will come from webcam
 cap=cv2.VideoCapture(0)
 
 while True:
@@ -50,9 +63,11 @@ while True:
     imgS=cv2.resize(img,(0,0),None,0.25,0.25)
     imgS=cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
 
+    #to tackle the case in wich we are goinf to find miultiple images in webcam we will firstly find the location of that image
     facesCurFrame=face_recognition.face_locations(imgS)
     encodesCurFrame=face_recognition.face_encodings(imgS,facesCurFrame)
 
+    #after getting encoding of both the imges here we are matching them
     for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
         matches=face_recognition.compare_faces(encodeListKnown,encodeFace)
         faceDis=face_recognition.face_distance(encodeListKnown,encodeFace)
@@ -67,6 +82,7 @@ while True:
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            #calling attendace function
             markAttendence(name)
 
     cv2.imshow('Webcam',img)
